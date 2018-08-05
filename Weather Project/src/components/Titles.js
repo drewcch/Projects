3773763307e828c47c672700540a6f032b58fa25
Andrew Chien <drewcch@gmail.com>
2018-08-05 14:33:26 -0700

    Add files via upload

---
+++ Weather Project/src/components/Titles.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+const Titles = () => (
+  <div>
+    <h1 className="title-container__title">Weather App</h1>
+    <h3 className="title-container__subtitle">
+      Enter a location to find out temperature, conditions and more!
+    </h3>
+  </div>
+);
+
+export default Titles;
